5b15323a79d97b1ea9601a4b9d1924759ce243d3
Vadim Shamray <shamray-95@bk.ru>
2024-11-27 18:02:58 +0700

    Tested some VirtualMachine's functionality.

--- src/slisp/main.cpp
+++ src/slisp/main.cpp
@@ -7,15 +7,33 @@
 
 using namespace Slisp;
 
+VirtualMachine::VirtualMachine vm;
+
+Types::Value* plus_func(std::vector<Types::Value*>& args) {
+    Types::Number* result = vm.push<Types::Number>();
+    for (auto const n : args) {
+        *result = *result + *static_cast<Types::Number*>(n);
+    }
+    return result;
+}
+
 int main(int argc, char **argv) {
     (void) argc;
     (void) argv;
 
-    VirtualMachine::VirtualMachine vm;
+    std::function<Types::Value*(std::vector<Types::Value*>&)>
+        plus_function = plus_func;
 
     auto* n1 = vm.track<Types::Number>(34);
     auto* n2 = vm.track<Types::Number>(35);
     auto* cons = vm.push<Types::Cons>(n1, n2);
+    auto* plus_proc = vm.push<Types::Procedure>(plus_function);
+    std::vector<Types::Value*> args { n1, n2 };
+    Types::Number sum_number =
+        *static_cast<Types::Number*>((*plus_proc)(args));
+    cons->set_cdr(nullptr);
+    cons->set_car(nullptr);
+    auto* sum = vm.push<Types::Number>(Types::Number { sum_number });
 
-    std::cout << "Slispy Lisp!\n";
+    std::cout << sum->to_string() << std::endl;
 }
