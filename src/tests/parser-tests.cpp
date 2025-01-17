2ee069fa09368942ce5ae58a3c65f8674d138b79
Vadim Shamray <shamray-95@bk.ru>
2025-01-17 16:36:30 +0700

    Added more complicated test case.

--- src/tests/parser-tests.cpp
+++ src/tests/parser-tests.cpp
@@ -7,7 +7,7 @@ using namespace Slisp::Types;
 using namespace Slisp::Parser;
 
 template <typename T>
-static Cons* to_cons_list(const T& lst) {
+static Cons* strs_to_list(const T& lst) {
     Cons* out = new Cons();
     Cons* last_cons = out;
 
@@ -21,6 +21,26 @@ static Cons* to_cons_list(const T& lst) {
     for (auto it = lst.begin() + 1; it != lst.end(); ++it) {
         Symbol* new_value = new Symbol(*it);
         Cons* new_cons = new Cons(new_value);
+        last_cons->set_cdr(new_cons);
+        last_cons = new_cons;
+    }
+
+    return out;
+}
+
+template <typename T>
+static Cons* vals_to_list(const T& lst) {
+    Cons* out = new Cons();
+    Cons* last_cons = out;
+
+    if (lst.begin() == lst.end()) {
+        return out;
+    }
+
+    out->set_car(*lst.begin());
+
+    for (auto it = lst.begin() + 1; it != lst.end(); ++it) {
+        Cons* new_cons = new Cons(*it);
         last_cons->set_cdr(new_cons);
         last_cons = new_cons;
     }
@@ -52,7 +72,7 @@ TEST_CASE("Pair") {
     std::string input = "(1 2)";
     std::vector<std::string> expected_values { "1", "2", };
 
-    Cons* expected = to_cons_list<std::vector<std::string>>(expected_values);
+    Cons* expected = strs_to_list<std::vector<std::string>>(expected_values);
     Lexer lxr { input };
 
     const Cons* result = dynamic_cast<const Cons*>(parse(lxr));
@@ -63,7 +83,7 @@ TEST_CASE("List") {
     std::string input = "(1 2 3 4 5)";
     std::vector<std::string> expected_values { "1", "2", "3", "4", "5", };
 
-    Cons* expected = to_cons_list<std::vector<std::string>>(expected_values);
+    Cons* expected = strs_to_list<std::vector<std::string>>(expected_values);
     Lexer lxr { input };
 
     const Cons* result = dynamic_cast<const Cons*>(parse(lxr));
@@ -74,7 +94,7 @@ TEST_CASE("List 2") {
     std::string input = "(1 2 3 4)";
     std::vector<std::string> expected_values { "1", "2", "3", "4", };
 
-    Cons* expected = to_cons_list<std::vector<std::string>>(expected_values);
+    Cons* expected = strs_to_list<std::vector<std::string>>(expected_values);
     Lexer lxr { input };
 
     const Cons* result = dynamic_cast<const Cons*>(parse(lxr));
@@ -85,7 +105,27 @@ TEST_CASE("List 3") {
     std::string input = " ( define name 'vadim ) ";
     std::vector<std::string> expected_values { "define", "name", "'vadim", };
 
-    Cons* expected = to_cons_list<std::vector<std::string>>(expected_values);
+    Cons* expected = strs_to_list<std::vector<std::string>>(expected_values);
+    Lexer lxr { input };
+
+    const Cons* result = dynamic_cast<const Cons*>(parse(lxr));
+    CHECK(result->equalp(expected));
+}
+
+TEST_CASE("Tree") {
+    std::string input = "(define (square x) (* x x))";
+
+    std::vector<std::string> sublist_1 { "square", "x" };
+    std::vector<std::string> sublist_2 { "*", "x", "x" };
+
+    std::vector<Value*> expected_values = {
+        new Symbol(std::string { "define"} ),
+        strs_to_list(sublist_1),
+        strs_to_list(sublist_2),
+    };
+
+    Cons* expected = vals_to_list(expected_values);
+
     Lexer lxr { input };
 
     const Cons* result = dynamic_cast<const Cons*>(parse(lxr));
